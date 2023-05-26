93b1038363a901deeedcd65755a2d13673c7fb12
Roman Antonio <67080400+romanisidor@users.noreply.github.com>
2023-05-26 02:41:57 -0600

    resume and projects section added

---
+++ js/index.js
@@ -0,0 +1,25 @@
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll('a[href^="#"]');
+    const sidebar = document.querySelector(".mobile_sidebar");
+    const hamburger = document.querySelector(".navbar__burguer-button");
+    let sidebarOpen = false;
+    hamburger.addEventListener("click", () => {
+        if (!sidebarOpen) {
+        sidebar.style.left = "0";
+        sidebarOpen = true;
+        } else {
+        sidebar.style.left = "-100%";
+        sidebarOpen = false;
+        }
+    });
+
+    links.forEach((link) => {
+        link.addEventListener("click", () => {
+            sidebar.style.left = "-100%";
+            sidebarOpen = false;
+        });
+    });
+    });
